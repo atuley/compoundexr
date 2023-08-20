008065ce371db446126989552664791336ecc461
Alex Tuley <alex.p.tuley@gmail.com>
2023-08-20 12:51:18 -0400

    `mix phx.gen.auth Accounts User users --binary-id`

---
+++ test/support/fixtures/accounts_fixtures.ex
@@ -0,0 +1,31 @@
+defmodule Compoundexr.AccountsFixtures do
+  @moduledoc """
+  This module defines test helpers for creating
+  entities via the `Compoundexr.Accounts` context.
+  """
+
+  def unique_user_email, do: "user#{System.unique_integer()}@example.com"
+  def valid_user_password, do: "hello world!"
+
+  def valid_user_attributes(attrs \\ %{}) do
+    Enum.into(attrs, %{
+      email: unique_user_email(),
+      password: valid_user_password()
+    })
+  end
+
+  def user_fixture(attrs \\ %{}) do
+    {:ok, user} =
+      attrs
+      |> valid_user_attributes()
+      |> Compoundexr.Accounts.register_user()
+
+    user
+  end
+
+  def extract_user_token(fun) do
+    {:ok, captured_email} = fun.(&"[TOKEN]#{&1}[TOKEN]")
+    [_, token | _] = String.split(captured_email.text_body, "[TOKEN]")
+    token
+  end
+end
