e15bb19bd80684866418c3f6f5c54bc4717b8380
Alex Tuley <alex.p.tuley@gmail.com>
2025-03-31 20:53:15 -0400

    Fix production build configuration

--- config/prod.exs
+++ config/prod.exs
@@ -6,7 +6,11 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :compoundexr, CompoundexrWeb.Endpoint,
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [host: System.get_env("PHX_HOST") || "localhost", port: 80],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  server: true,
+  root: ".",
+  version: Application.spec(:compoundexr, :vsn)
 
 # Configures Swoosh API Client
 config :swoosh, api_client: Swoosh.ApiClient.Finch, finch_name: Compoundexr.Finch
